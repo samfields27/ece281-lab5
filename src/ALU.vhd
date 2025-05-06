853af8fbe6631876c02824910112300e1935edef
Fields <c27samuel.fields@afacademy.af.edu>
2025-05-05 22:37:34 -0600

    Attempt 4

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -74,7 +74,10 @@ begin
     ripple_adder0: ripple_adder
         port map(
             A=>i_A(3 down to 0),
-            B=>numB(3 downto 0)
+            B=>numB(3 downto 0),
+            Cin=>carry(0),
+            S=>total(3 downto 0),
+            Cout=>carry(4) );
 
 
 
